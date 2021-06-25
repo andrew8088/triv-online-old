0333a3f278f47de0310f416b44485308fe481100
Andrew Burgess <andrew.burgess@classdojo.com>
2021-06-25 02:26:24 +0000

    add slack.conversations.members

--- lib/slack/conversations.ts
+++ lib/slack/conversations.ts
@@ -12,3 +12,19 @@ export const info = (channel: string) => {
     },
   });
 };
+
+export const members = (channel: string, cursor?: string, limit?: string) => {
+  const params: Record<string, any> = { channel };
+
+  if (cursor) params.cursor = cursor;
+  if (limit) params.limit = limit;
+
+  return axios({
+    method: "GET",
+    url: `${BASE_URL}/conversations.members?${stringify({ channel })}`,
+    headers: {
+      "Content-Type": "application/x-www-form-urlencoded",
+      Authorization: `Bearer ${API_TOKEN}`,
+    },
+  });
+};
--- test/slack/conversations.test.ts
+++ test/slack/conversations.test.ts
@@ -1,16 +1,28 @@
-import { strictEqual as equal } from "assert";
+import { ok, strictEqual as equal } from "assert";
 import * as slack from "../../lib/slack";
 import { channels } from "../fixtures";
 
+const { public1 } = channels;
+
 describe("conversations", () => {
   describe("info", () => {
-    it("should succeed", async () => {
-      const { public1 } = channels;
+    it("200s", async () => {
       const { data, status } = await slack.conversations.info(public1.id);
 
       equal(status, 200);
+      ok(data.ok);
       equal(data.channel.id, public1.id);
       equal(data.channel.name, public1.name);
+    });
+  });
+
+  describe("members", () => {
+    it("200s", async () => {
+      const { data, status } = await slack.conversations.members(public1.id);
+
+      equal(status, 200);
+      ok(data.ok);
+      ok(data.members.length > 0);
     });
   });
 });

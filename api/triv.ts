6b786f1d85305865e00df088d57e0ae7f020f1d8
Andrew Burgess <andrew@burgess.io>
2021-06-25 22:01:41 -0400

    remove members call to test modal

--- api/triv.ts
+++ api/triv.ts
@@ -16,54 +16,54 @@ export default async (request: VercelRequest, response: VercelResponse) => {
     return response.status(200).send(channel("Sorry, the `" + text + "` command is not supported yet"));
   }
 
-  const data = await slack.conversations.members({ channel: channel_id });
+  // const data = await slack.conversations.members({ channel: channel_id });
 
-  if (data.ok) {
-    response.status(200).send(channel(`Okay, preparing your trivia game!`));
+  //if (data.ok) {
+  response.status(200).send(channel(`Okay, preparing your trivia game!`));
 
-    const result = await slack.views.open({
-      trigger_id,
-      view: {
-        type: "modal",
-        title: {
-          type: "plain_text",
-          text: "Triv.Online",
-        },
-        blocks: [
-          {
-            type: "section",
-            text: {
-              type: "mrkdwn",
-              text: "Welcome to _Triv.Online_",
-            },
-            accessory: {
-              type: "button",
-              text: {
-                type: "plain_text",
-                text: "Click me!",
-              },
-              action_id: "button_abc",
-            },
-          },
-          {
-            type: "input",
-            block_id: "input_c",
-            label: {
-              type: "plain_text",
-              text: "Players",
-            },
-            element: {
-              type: "plain_text_input",
-              action_id: "dreamy_input",
-              multiline: true,
-            },
-          },
-        ],
+  const result = await slack.views.open({
+    trigger_id,
+    view: {
+      type: "modal",
+      title: {
+        type: "plain_text",
+        text: "Triv.Online",
       },
-    });
-    console.log(result);
-    return;
-  }
+      blocks: [
+        {
+          type: "section",
+          text: {
+            type: "mrkdwn",
+            text: "Welcome to _Triv.Online_",
+          },
+          accessory: {
+            type: "button",
+            text: {
+              type: "plain_text",
+              text: "Click me!",
+            },
+            action_id: "button_abc",
+          },
+        },
+        {
+          type: "input",
+          block_id: "input_c",
+          label: {
+            type: "plain_text",
+            text: "Players",
+          },
+          element: {
+            type: "plain_text_input",
+            action_id: "dreamy_input",
+            multiline: true,
+          },
+        },
+      ],
+    },
+  });
+  console.log(result);
+  return;
+  //}
 
   return response.status(200).send(channel("Sorry, something went wrong."));
 };
